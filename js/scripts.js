77baf79e21bab5737713e326d0c3986b6959f5be
Student <example@example.com>
2019-02-04 19:28:10 -0800

    add some functions fot the walrus

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+
+$(document).ready(function() {
+
+$("h1").click(function(){
+  alert("This is a headre");
+});
+
+$("img").click(function() {
+  alert ("This is an image");
+});
+
+$("p").click(function(){
+  alert("This is a paragraph");
+});
+
+
+
+
+
+
+
+});
